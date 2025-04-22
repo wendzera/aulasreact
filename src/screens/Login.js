2bfc56f0b57f3a73b55cc6d6a05f90ca6ad3e777
Wendzera <wendell.nascimento2006@gmail.com>
2025-04-22 15:31:33 -0300

    criação de tela login 22-04

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
-//import styles from "./Login.module.css";
+import styles from "./Login.module.css";
+
 const Login = () => {
     return (
-        <div>Faça seu login</div>
-    )
+        <div>
+            <h2>Faça seu login</h2>
+            <form>
+                <label>
+                    <span>E-mail:</span>
+                    <input type="text" name="displayName" required placeholder="E-mail" />
+                </label>
+                
+                <label>
+                    <span>Senha:</span>
+                    <input type="password" name="password" required placeholder="Senha" />
+                </label>
+                
+                <button className="btnEntrar">
+                    Entrar
+                </button>
+            </form>
+        </div>
+    );
 }
- 
-export default Login
+
+export default Login;
